262e8ab6b28c9dbde6bea67f9850da42e273faee
Bargan Nicolai <41567806+Nikba-Creative-Studio@users.noreply.github.com>
2021-04-07 23:06:00 +0300

    v 1.91
    - Viber issue resolved

--- contactus.js
+++ contactus.js
@@ -14,7 +14,7 @@ let messengers = {
         label: 'Write a message'
     },
     "viber": {
-        url: 'viber://chat?number=',
+        url: 'viber://contact?number=',
         label: 'Write a message'
     },
     "telegram": {
